a412db6629121004a32e3def5d246f4150bb2191
RuslanGabdulkhaev <prorush@Yandex.ru>
2026-04-22 11:01:58 +0500

    Углубление в playwright

---
+++ playwright_errors.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    # Открываем браузер и создаем новую страницу
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login",
+              wait_until="networkidle",
+              )
+
+
+    # unknow = page.locator('#unknow')
+    # expect(unknow).to_be_visible()
+
+    # login_button = page.get_by_test_id('login-page-login-button')
+    # login_button.fill('unknow')
+
+    page.evaluate(
+        """
+        (text) => {
+            const title = document.getElementById('authentication-ui-course-title-text')
+            title.textContent = 'text'
+        }
+        """
+    )
+
+    page.wait_for_timeout(5000)
