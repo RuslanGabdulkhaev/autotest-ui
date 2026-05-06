1946536b8616f027452cfc0927e9e5ddc35befed
RuslanGabdulkhaev <prorush@Yandex.ru>
2026-05-06 20:31:05 +0500

    Интеграция pytest и playwright

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 def test_successful_registration():
     with sync_playwright() as playwright:
@@ -28,9 +28,14 @@ def test_successful_registration():
         context.storage_state(path='browser-state.json')
 
     with sync_playwright() as playwright:
-        # Открываем браузер и создаем новую страницу
+        # Открываем браузер и открываем новую страницу
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context(storage_state='browser-state.json')
         page = context.new_page()
 
         page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
+        # Проверяем, что существует заголовок "Dashboard"
+        dashboard_headers = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(dashboard_headers).to_be_visible()
+        expect(dashboard_headers).to_have_text("Dashboard")
